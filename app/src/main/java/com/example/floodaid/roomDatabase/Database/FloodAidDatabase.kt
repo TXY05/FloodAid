8c07d70921ee385eaa64f912a65e0a824c309e43
BaldPancake <alasfong1106@gmail.com>
2025-05-02 12:50:03 +0800

    Update FloodAidDatabase.kt

--- app/src/main/java/com/example/floodaid/roomDatabase/Database/FloodAidDatabase.kt
+++ app/src/main/java/com/example/floodaid/roomDatabase/Database/FloodAidDatabase.kt
@@ -5,15 +5,26 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.floodaid.models.VolunteerEvent
+import com.example.floodaid.models.VolunteerEventHistory
 import com.example.floodaid.utils.Converter
 import com.example.floodaid.roomDatabase.Dao.MapDao
+import com.example.floodaid.roomDatabase.Dao.VolunteerDao
+import com.example.floodaid.roomDatabase.Dao.VolunteerEventHistoryDao
 import com.example.floodaid.roomDatabase.Entities.District
 import com.example.floodaid.roomDatabase.Entities.FloodMarker
 import com.example.floodaid.roomDatabase.Entities.Shelter
 import com.example.floodaid.roomDatabase.Entities.State
 
 @Database(
-    entities = [State::class, District::class, Shelter::class, FloodMarker::class],
+    entities = [
+        State::class,
+        District::class,
+        Shelter::class,
+        FloodMarker::class,
+        VolunteerEvent::class,
+        VolunteerEventHistory::class
+    ],
     version = 2, // Increment this number by 1
     exportSchema = true
 )
@@ -21,6 +32,8 @@ import com.example.floodaid.roomDatabase.Entities.State
 abstract class FloodAidDatabase : RoomDatabase() {
 
     abstract fun MapDao(): MapDao
+    abstract fun volunteerDao(): VolunteerDao
+    abstract fun volunteerEventHistoryDao(): VolunteerEventHistoryDao
 
     companion object {
         @Volatile
